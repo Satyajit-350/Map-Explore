6d6156df0a6f5e6737169a888999678cc8d5fb91
Satyajit Biswal <86532832+Satyajit-350@users.noreply.github.com>
2023-06-13 17:39:12 +0530

    Security changes

--- app/src/main/java/com/example/assignment/utils/Constants.kt
+++ app/src/main/java/com/example/assignment/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.example.assignment.utils
 object Constants {
 
     const val BASE_URL = "https://maps.googleapis.com/maps/api/place/"
-    const val API_KEY = "AIzaSyA3zeQUA47kyCgI5XJFJMn6zybxb3jPqeQ"
+    const val API_KEY = "YOUR API KEY"
     const val PLACE_AUTOCOMPLETE_RADIUS = "3000"
     const val NEARBY_RADIUS = 1000
     const val PLACE_AUTOCOMPLETE_COMPONENT = "country:in"
